47cba20b29afaf26cdf750526493811a82109e4c
m_daniyal <danik@magomedow@mail.ru>
2022-11-18 21:29:11 +0300

    Добавьте файлы проекта.

---
+++ TetrisClientNorm/Client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Accessibility;
+
+namespace TetrisClientNorm
+{
+
+    public class Client
+    {
+        Socket client;
+        
+        public Client()
+        {
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void ConnectServer(String ip, int port)
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            client.Connect(ipEndPoint);
+        }
+
+        public async void SendMessage(String message)
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message + '\n');
+            await client.SendAsync(messageBytes, SocketFlags.None);
+        }
+
+        public string ReceiveResponse()
+        {
+            var response = new List<byte>();
+            var bytesRead = new byte[1];
+
+            while (true)
+            {
+                var count = client.Receive(bytesRead);
+                if (count == 0 || bytesRead[0] == '\n') break;
+
+                response.Add(bytesRead[0]);
+            }
+
+            return Encoding.UTF8.GetString(response.ToArray());
+        }
+
+        public void DisconnectServer()
+        {
+            client.Shutdown(SocketShutdown.Both);
+        }
+
+    }
+}
---
+++ TetrisClientNorm/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TetrisClientNorm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly ImageSource[] tileImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/TileEmpty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileCyan.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileOrange.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileRed.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileYellow.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileGreen.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TilePurple.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileBlue.png", UriKind.Relative))
+        };
+
+        private readonly ImageSource[] blockImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/Block-Empty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-I.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-J.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-L.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-O.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-S.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-T.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-Z.png", UriKind.Relative))
+        };
+
+
+        public static int Column = 10;
+        public static int Row = 20;
+
+        private readonly Image[,] imageControls;
+
+        private Client client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            imageControls = SetupGameCanvas();
+            StartGame();
+        }
+
+        public async Task GameLoop()
+        {
+            while (true)
+            {
+                DrawGrid();
+                DrawBlock();
+                await Task.Delay(500);
+            }
+        }
+
+        private void StartGame()
+        {
+            client = new Client();
+            client.ConnectServer("127.0.0.1", 333);
+
+            client.SendMessage($"StartGame {Row} {Column}" + '\n');
+        }
+
+
+        private Image[,] SetupGameCanvas()
+        {
+            Image[,] imageControls = new Image[Row, Column];
+            int cellSize = 25;
+
+            for (int i = 0; i < Row; i++)
+            {
+                for (int j = 0; j < Column; j++)
+                {
+                    Image imageControl = new Image()
+                    {
+                        Width = cellSize,
+                        Height = cellSize
+                    };
+
+                    Canvas.SetTop(imageControl, i*cellSize);
+                    Canvas.SetLeft(imageControl, j*cellSize);
+                    GameCanvas.Children.Add(imageControl);
+                    imageControls[i, j] = imageControl;
+                }
+            }
+
+            return imageControls;
+        }
+
+
+        private void DrawGrid()
+        {
+            for (int i = 0; i < Row; i++)
+            {
+                for (int j = 0; j < Column; j++)
+                {
+                    client.SendMessage($"GetGrid {i} {j}" + '\n');
+
+                    int id = Convert.ToInt32(client.ReceiveResponse());
+                    imageControls[i, j].Source = tileImages[id];
+                }
+            }
+        }
+
+        private void DrawBlock()
+        {
+            client.SendMessage($"GetBlock" + '\n');
+
+            string blockPositions = client.ReceiveResponse();
+
+            var id = Convert.ToInt32(blockPositions.Split('n').Last());
+            for (int i = 0; i < blockPositions.Split('n').Length-1; i++)
+            {
+                var block = blockPositions.Split('n')[i];
+                var row = Convert.ToInt32(block.Split('-')[0]);
+                var col = Convert.ToInt32(block.Split('-')[1]);
+
+                imageControls[row, col].Source = tileImages[id];
+            }
+        }
+
+
+
+        private async void GameCanvas_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            await GameLoop();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
